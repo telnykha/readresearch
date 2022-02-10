4594052853d6d6398791060ca39aecdd93a62e0f
tyf2 <95036132+tyf2@users.noreply.github.com>
2022-02-10 18:55:07 +0400

    img to feature vector
    1- N levels
    2-7 - features (A..C)
    8 - folder with images
    9 - output path
    10 - N video
    11 - N images/video

---
+++ Img2featureVector/Img2featureVector/Img2featureVector.cpp
@@ -0,0 +1,80 @@
+// Img2featureVector.cpp: определяет точку входа для консольного приложения.
+//
+// Player.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+extern "C"
+{
+//#pragma comment(lib, "awpipl2.lib")
+//#pragma comment(lib, "awplflib.lib")
+}
+
+int Nout = 300;
+void features(char* argv[], int sxbase, int sybase, int wbase, int hbase, TLFObjectList &list);
+void tiles(char* argv[], int Levels, int sx, int sy, int wx, int wy, TLFObjectList &list);
+
+int main(int argc, char* argv[])
+{
+	if (argc < 10) return 2;
+		char FName[60];
+		TLFImage img;
+		ofstream sink;
+		TLFObjectList list;
+		tiles(argv, atoi(argv[1]), 0, 0, 159, 89, list);
+		int Ncolumns = list.GetCount();
+		sink.open(argv[8]);
+		for (int VNumb = 1; VNumb <= atoi(argv[10]); VNumb++){
+			for (int ImgNum = 1; ImgNum <= atoi(argv[11]); ImgNum++){
+				sink << VNumb << ";" << ImgNum << ";";
+				sprintf_s(FName, "%s\\%d_%d.awp",argv[9], VNumb, ImgNum);
+				img.LoadFromFile(FName);
+				for (int Column = 0; Column < Ncolumns; Column++){
+					ILFFeature *f = (ILFFeature*)list.Get(Column);
+					sink << f->fCalcValue(&img) << ";";
+				}
+				sink << endl;
+			}
+		}
+		sink.close();
+	_getch();
+	return 0;
+}
+
+void features(char* argv[], int sxbase, int sybase, int wbase, int hbase, TLFObjectList &list){
+	if (strcmp(argv[2], "1") == 0){
+		TLFAFeature *f = new TLFAFeature(sxbase, sybase, wbase, hbase);
+		list.Add(f);
+	}
+	if (strcmp(argv[3], "1") == 0){
+		TLFSFeature *f = new TLFSFeature(sxbase, sybase, wbase, hbase);
+		list.Add(f);
+	}
+	if (strcmp(argv[4], "1") == 0){
+		TLFHFeature *f = new TLFHFeature(sxbase, sybase, wbase, hbase / 2);
+		list.Add(f);
+	}
+	if (strcmp(argv[5], "1") == 0){
+		TLFVFeature *f = new TLFVFeature(sxbase, sybase, wbase / 2, hbase);
+		list.Add(f);
+	}
+	if (strcmp(argv[6], "1") == 0){
+		TLFDFeature *f = new TLFDFeature(sxbase, sybase, wbase / 2, hbase / 2);
+		list.Add(f);
+	}
+	if (strcmp(argv[7], "1") == 0){
+		TLFCFeature *f = new TLFCFeature(sxbase, sybase, wbase, hbase);
+		list.Add(f);
+	}
+}
+void tiles(char* argv[], int Levels, int sx, int sy, int wx, int wy, TLFObjectList &list){
+	features(argv, sx, sy, wx, wy,list);
+	if (Levels > 0){
+		Levels--;
+		tiles(argv, Levels, sx, sy, wx / 2, wy / 2, list);
+		tiles(argv, Levels, sx + wx / 2, sy, wx / 2, wy / 2, list);
+		tiles(argv, Levels, sx, sy + wy / 2, wx / 2, wy / 2, list);
+		tiles(argv, Levels, sx + wx / 2, sy + wy / 2, wx / 2, wy / 2, list);
+	}
+}
---
+++ Img2featureVector/Img2featureVector/stdafx.h
@@ -0,0 +1,27 @@
+// stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
+// или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
+// не часто измен€ютс€
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <conio.h>
+#include <tchar.h>
+#include "_LF.h"
+#include <fstream>
+#include "..\..\..\..\IPF\common\cvvideo\module\awpcvvideo\awpcvvideo.h"
+#include <windows.h>
+#include <direct.h>
+#include <cstdlib>
+
+
+//using std::string;
+
+
+
+
+
+// TODO: ”становите здесь ссылки на дополнительные заголовки, требующиес€ дл€ программы
